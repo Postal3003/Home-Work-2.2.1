0003b3e947f2e31d323cad004a3214c1c00f5b5d
Dmitry <borodulkin_d@mail.ru>
2022-05-13 15:29:34 +0300

    Improved constraints.

--- Home Work 2.2.1/ViewController.swift
+++ Home Work 2.2.1/ViewController.swift
@@ -29,12 +29,49 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
      
-//        setColor()
-//        setValue(for: redLabel, greenLabel, blueLabel)
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+    }
+    // MARK: - IB Actions
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        
+        switch sender {
+        case redSlider: setValue(for: redLabel)
+        case greenSlider: setValue(for: greenLabel)
+        default: setValue(for: blueLabel)
+        }
+        
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
 
-    // MARK: IB Actions
-    
-
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                label.text = string(from: redSlider)
+            case greenLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
+    }
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
+
+
+
+
+
+
 
